714c07b5800f0e29bf33da5e0f2340d48eafd194
bigcjat <33845018+bigcjat@users.noreply.github.com>
2024-01-06 01:39:52 -0800

    Update toml.php
    Added allowedIP

--- webhost/toml.php
+++ webhost/toml.php
@@ -4,9 +4,19 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 // You can change these values
+$allowedIPAddress = '0.0.0.0'; // Replace with your validator IP address
 $apiKey = 'key'; // Set your API key here, this can be anything you want
-$filePath = '.well-known/xahau.toml'; // File path relitive to this PHP script. Change xahau.toml to xrp-ledger.toml 
-if you are using the Mainnet
+$filePath = '.well-known/xahau.toml'; // File path relitive to this PHP script. Change xahau.toml to xrp-ledger.toml if you are using the Mainnet
+
+// Get the client's IP address
+$clientIPAddress = $_SERVER['REMOTE_ADDR'];
+
+// Check if the client's IP address matches the allowed IP address
+if ($clientIPAddress !== $allowedIPAddress) {
+    // If not, exit the script with an unauthorized error message
+    http_response_code(401); // Set HTTP response code to 401 Unauthorized
+    die('Error: Unauthorized access');
+}
 
 if (!isset($_GET['apiKey']) || $_GET['apiKey'] !== $apiKey) {
     http_response_code(401);
